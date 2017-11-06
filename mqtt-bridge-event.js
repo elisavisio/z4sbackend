7c01d91cfe90288c86ec0e9e316a5afb530ae5f9
elisavisio <elisavisio@gmail.com>
2017-11-06 10:33:21 +0100

    add devices

--- mqtt-bridge-event.js
+++ mqtt-bridge-event.js
@@ -5,17 +5,13 @@
  * license which can be found in the file 'LICENSE.txt' in this package distribution
  * or at 'https://opensource.org/licenses/BSD-3-Clause'.
  */
+var devices = require("./devices.js")
 var request = require('request')
 var mqtt = require('mqtt')
 const urlmqtt = "mqtt://liveobjects.orange-business.com:1883"
 const apiKey = "0d1bde96f14e4e7b926a10ff5f3b3b7a"
 
-
-
-const eventTopic = "router/~event/v1/data/eventprocessing/#"
-const connectTopic = "router/~event/v2/assets/#"
 const routesTopic =  {"router/~event/v1/data/new":0,"router/~event/v1/data/eventprocessing/#": 0, "router/~event/v2/assets/#": 0}
-console.log(routesTopic)
 /** connect **/
 console.log("MQTT::Connecting to ");
 var client  = mqtt.connect(urlmqtt, {username:"payload", password:apiKey, keepAlive:30})
@@ -23,7 +19,6 @@ var client  = mqtt.connect(urlmqtt, {username:"payload", password:apiKey, keepAl
 /** client on connect **/
 client.on("connect", function() {
   console.log("MQTT::Connected");
-
   client.subscribe(routesTopic)
   console.log("MQTT::Subscribed to topic:", routesTopic);
 })
@@ -48,15 +43,17 @@ client.on("offline", function(err) {
   console.log("MQTT::offline from client --> ", err);
 })
 
-
 client.on("message", function (topic, message) {
-
-  console.log("MQTT::New message\n");
-  
-  //var eventMessage = JSON.parse(message)
-  console.log(topic);
- 
-
+    
+  if(topic.includes("/connected")){
+    console.log("device connected");
+  }else if(topic.includes("/disconnected")){
+    console.log("device disconnected");
+  }else if(topic.includes("router/~event/v1/data/new")){
+    console.log("new data");
+  }else if(topic.includes("router/~event/v1/data/eventprocessing/")){
+    console.log("new event");
+  }
 })
 
 
