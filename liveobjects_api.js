7973859a2049557bd7f37ea2aef8c58b1d6362c5
elisavisio <elisavisio@gmail.com>
2017-11-06 11:36:24 +0100

    proxy request

--- liveobjects_api.js
+++ liveobjects_api.js
@@ -1,12 +1,38 @@
 var request = require('request');
 
-var getBeacons = function() {
+var getBeacons = function () {
     return {};
 }
 
-var getSensors = function(callback,response) {
-    callback(response,{"message":"ok"});
-    return true;
+var getSensors = function (callback, responseHandler) {
+    //    callback(response,{"message":"ok"});
+    var options = {
+        //url: 'https://liveobjects.orange-business.com/api/v0/assets?size=20&connected=true',
+        url: 'https://liveobjects.orange-business.com/api/v0/assets?size=20',
+        headers: {
+            'User-Agent': 'request',
+            'X-API-KEY': '0d1bde96f14e4e7b926a10ff5f3b3b7a'
+        }
+    };
+
+    function devicesConnected(error, response, body) {
+        var devices = [];
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            //console.log(info.totalCount);
+            info.data.forEach(function (element) {
+                //console.log(element.namespace+element.id);
+                devices.push("urn:lo:nsid:" + element.namespace + ':' + element.id);
+
+            }, this);
+            callback(responseHandler,devices);
+            //console.log(devices);
+
+
+        }
+    }
+
+    request(options, devicesConnected);
 }
 
 exports.getBeacons = getBeacons;
